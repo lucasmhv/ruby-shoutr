a144262bdaa192bea605ffe00a8f86cc3977563e
Lucas Valadares <lucasmhv@gmail.com>
2019-05-08 17:58:17 -0300

    Refactoring the shouts forms

--- app/helpers/shouts_helper.rb
+++ app/helpers/shouts_helper.rb
@@ -1,4 +1,12 @@
 module ShoutsHelper
+
+  def shout_form_for(content_type)
+    form_for Shout.new do |form|
+      form.hidden_field(:content_type, value: content_type) +
+      form.fields_for(:content) { |content_form| yield(content_form) } +
+      form.submit("shout!")
+    end
+  end
   
   def like_button(shout)
     if current_user.liked?(shout)
