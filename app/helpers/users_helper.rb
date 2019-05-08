ee7fe4fb1ce259bb8d8f559f1f46982c1bb40a8a
Lucas Valadares <lucasmhv@gmail.com>
2019-05-08 15:23:10 -0300

    Moved avatar functionality to UserHelper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  
+  def avatar(user)
+    email_digest = Digest::MD5.hexdigest(user.email)
+    gravatar_url = "//www.gravatar.com/avatar/#{email_digest}"
+
+    image_tag gravatar_url
+  end
 end
