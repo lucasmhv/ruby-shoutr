0ce807fbadfe0e9271b37404c268e99894c9f265
Lucas Valadares <lucasmhv@gmail.com>
2019-05-09 15:56:09 -0300

    Allowing to search on text body

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
 
   resource :session, only: [:create]
+  resource :search, only: [:show]
 
   resources :users, only: [:create, :show] do
     resources :followers, only: [:index]
