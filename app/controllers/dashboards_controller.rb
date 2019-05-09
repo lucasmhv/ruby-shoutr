0e7eae55d13030ad05067f988daf5b714da0c481
Lucas Valadares <lucasmhv@gmail.com>
2019-05-09 14:46:23 -0300

    Extracted Timeline and Dashboard classes using single responsability principle

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,8 +1,7 @@
 class DashboardsController < ApplicationController
 
   def show
-    @shout = Shout.new
-    @shouts = current_user.timeline_shouts
+    @dashboard = Dashboard.new(current_user)
   end
 
 end
--- app/models/shout.rb
+++ app/models/shout.rb
@@ -1,10 +1,6 @@
 class Shout < ApplicationRecord
   belongs_to :user
   belongs_to :content, polymorphic: true
-
   delegate :username, to: :user
-
-  validates :user, presence: true
-
-  default_scope { order(created_at: :desc) }  
+  validates :user, presence: true 
 end
